c0872de483b39042565f11fb6312822e1a369c6b
barucE <baruquinho_13@hotmail.com>
2020-12-22 00:45:12 -0600

    changed styles all pages. Implemented send email (contact)

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,7 +7,7 @@ import { NavController, Slides } from 'ionic-angular';
 })
 export class HomePage {
   @ViewChild(Slides) slides: Slides;
-  images = ["https://investorplace.com/wp-content/uploads/2016/10/insurancemsn2.jpg","http://directlineinsurance.ca/wp-content/uploads/2016/09/famins-1.jpg", "https://blog.securenow.in/wp-content/uploads/2017/11/top-up-health-insurance-1-800x445.jpg"]
+  images = ["./assets/imgs/slides/family.jpg","./assets/imgs/slides/insurance-health.jpg","./assets/imgs/slides/car.jpg","./assets/imgs/slides/hands.jpg","./assets/imgs/slides/money.jpg"]
   header_data:any;
   constructor(public navCtrl: NavController) {
   }
